58d3c977f5bdfda2e40603a3648f0502c57ea782
BasvVeen2 <bas.van.veen2@ah.nl>
2025-07-15 14:51:32 +0200

    why

--- bin/clean-example-data.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-WEB_REQUESTS_DIR=tests/data/web_requests
-
-find $WEB_REQUESTS_DIR/_delta_log -type f -not -name '00000000000000000000.json' -exec rm {} +
-find $WEB_REQUESTS_DIR -type d -name 'date=*' -exec rm -rf {} +
-
--- bin/extract-example-json.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-tar -xzvf tests/json/web_requests-100K.json.tar.gz -C tests/json
-
